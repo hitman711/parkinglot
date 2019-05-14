9dc370be8ceb4a078c184c138dad0599511aec0b
Siddhesh Gore <siddhesh.gore@100grams.nl>
2019-05-14 09:47:02 +0200

    Timezone correction

--- parking/tasks.py
+++ parking/tasks.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from django.utils import timezone
 from parkinglot.celery import app
 
 from . import models
@@ -9,13 +10,13 @@ def check_reservation_status():
     """ """
 
     for reservation in models.Reservation.objects.filter(
-        book_from__date=datetime.utcnow().date(),
+        book_from__date=timezone.now().date(),
         status__in=[
             models.Reservation.ACTIVE,
             models.Reservation.PENDING]
     ):
         if (
-            reservation.book_to < datetime.utcnow()
+            reservation.book_to < timezone.now()
         ) and reservation.status == models.Reservation.ACTIVE:
             reservation.status = models.Reservation.OVERDUE
             reservation.overdue_amount = (
@@ -24,8 +25,8 @@ def check_reservation_status():
             reservation.save()
 
         elif (
-            reservation.book_from < datetime.utcnow()
-            and reservation.book_to > datetime.utcnow()
+            reservation.book_from < timezone.now()
+            and reservation.book_to > timezone.now()
         ) and reservation.status == models.Reservation.PENDING:
             reservation.status = models.Reservation.ACTIVE
             reservation.save()
