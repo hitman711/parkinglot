2bcf2121ad80c6f6808cf66f224bc5a15692a303
Siddhesh Gore <siddhesh.gore@100grams.nl>
2019-05-14 09:39:31 +0200

    UTC time comparision added

--- parking/tasks.py
+++ parking/tasks.py
@@ -3,28 +3,29 @@ from parkinglot.celery import app
 
 from . import models
 
+
 @app.task
 def check_reservation_status():
     """ """
 
     for reservation in models.Reservation.objects.filter(
-        book_from__date=datetime.now().date(),
+        book_from__date=datetime.utcnow().date(),
         status__in=[
             models.Reservation.ACTIVE,
             models.Reservation.PENDING]
     ):
         if (
-            reservation.book_to < datetime.now()
+            reservation.book_to < datetime.utcnow()
         ) and reservation.status == models.Reservation.ACTIVE:
             reservation.status = models.Reservation.OVERDUE
             reservation.overdue_amount = (
                 reservation.overdue_amount + reservation.venue.price.overdue_amount)
             reservation.total_amount = reservation.total_amount + reservation.overdue_amount
             reservation.save()
-        
+
         elif (
-            reservation.book_from < datetime.now()
-            and reservation.book_to > datetime.now()
+            reservation.book_from < datetime.utcnow()
+            and reservation.book_to > datetime.utcnow()
         ) and reservation.status == models.Reservation.PENDING:
             reservation.status = models.Reservation.ACTIVE
             reservation.save()
